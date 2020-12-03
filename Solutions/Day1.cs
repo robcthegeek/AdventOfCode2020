0c5803c9f818c810dc33d0ef36c94b24d6f04e0f
Rob Cooper <robc.the.geek@gmail.com>
2020-12-03 09:52:41 +0000

    Day 1 ⭐⭐

--- Solutions/Day1.cs
+++ Solutions/Day1.cs
@@ -5,16 +5,31 @@ namespace Solutions
 {
     public static class Day1
     {
-        public static int Solve(string input)
+        public static int Part1(string input)
         {
             var ints = input
                 .Split(Environment.NewLine)
                 .Select(int.Parse)
                 .ToArray();
             
-            foreach (var a in ints)
+            foreach (var a in ints.Where(a => a <= 2020))
                 foreach (var b in ints)
                     if (a + b == 2020) return a * b;
+
+            throw new Exception("Couldn't find it!");
+        }
+
+        public static int Part2(string input)
+        {
+            var ints = input
+                .Split(Environment.NewLine)
+                .Select(int.Parse)
+                .ToArray();
+
+            foreach (var a in ints.Where(a => a <= 2020))
+                foreach (var b in ints.Where(b => a + b <= 2020))
+                    foreach (var c in ints)
+                        if (a + b + c == 2020) return a * b * c;
 
             throw new Exception("Couldn't find it!");
         }
--- Tests/Day1Tests.cs
+++ Tests/Day1Tests.cs
@@ -7,7 +7,7 @@ namespace Tests
     public class Day1Tests
     {
         [Fact]
-        public void Example_Yields_Correct_Result()
+        public void Part1_Example_Yields_Correct_Result()
         {
             var input = new []
             {
@@ -19,9 +19,27 @@ namespace Tests
                 "1456",
             }.Joined();
 
-            var result = Day1.Solve(input);
+            var result = Day1.Part1(input);
 
             Assert.Equal(514579, result);
+        }
+
+        [Fact]
+        public void Part2_Example_Yields_Correct_Result()
+        {
+            var input = new[]
+            {
+                "1721",
+                "979",
+                "366",
+                "299",
+                "675",
+                "1456",
+            }.Joined();
+
+            var result = Day1.Part2(input);
+
+            Assert.Equal(241861950, result);
         }
 
         [Fact]
@@ -29,9 +47,19 @@ namespace Tests
         {
             var input = Input.Day(1);
 
-            var result = Day1.Solve(input);
+            var result = Day1.Part1(input);
 
             Assert.Equal(1016131, result);
+        }
+
+        [Fact]
+        public void Solve_Part_2()
+        {
+            var input = Input.Day(1);
+
+            var result = Day1.Part2(input);
+
+            Assert.Equal(276432018, result);
         }
     }
 }

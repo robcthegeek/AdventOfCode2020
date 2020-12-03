cf20286022b8ecb4e0671b3b2e58cc4bf6446d30
Rob Cooper <robc.the.geek@gmail.com>
2020-12-03 09:18:05 +0000

    File > New Project

---
+++ Tests/Day1Tests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Tests
+{
+    public class Day1Tests
+    {
+        [Fact]
+        public void Tests_Work()
+        {
+            Assert.True(true);
+        }
+    }
+}

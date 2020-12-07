bbe58d39da7a016dfcc1f5623dacd06054b8382e
Rob Cooper <robc.the.geek@gmail.com>
2020-12-07 11:10:34 +0000

    Day 3 ⭐⭐

--- Solutions/Day3.cs
+++ Solutions/Day3.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Solutions
@@ -18,9 +20,23 @@ namespace Solutions
             });
         }
 
-        public static int Part2(string input)
+        public static ulong Part2(string input, IEnumerable<(int slopeX, int slopeY)> slopes) => slopes
+                .Select(slope => CountTrees(input, slope.slopeX, slope.slopeY))
+                .Aggregate(1UL, (x, y) => x * y);
+
+        private static uint CountTrees(string map, int slopeX, int slopeY)
         {
-            return -1;
+            var lines = map.Split(Environment.NewLine);
+            var lineLen = lines[0].Length;
+            uint count = 0;
+            for (var y = slopeY; y < lines.Length; y+=slopeY)
+            {
+                var x = ((y/slopeY) * slopeX);
+                x -= ((x / lineLen) * lineLen); // Shift if off edge of map
+                if (lines[y][x] == '#') count++;
+            }
+
+            return count;
         }
     }
 }
--- Tests/Day3Tests.cs
+++ Tests/Day3Tests.cs
@@ -1,3 +1,4 @@
+using System;
 using Solutions;
 using Xunit;
 
@@ -28,7 +29,7 @@ namespace Tests
             Assert.Equal(7, result);
         }
 
-        [Fact(Skip="WIP")]
+        [Fact]
         public void Part2_Example_Yields_Correct_Result()
         {
             var input = new[]
@@ -46,9 +47,16 @@ namespace Tests
                 ".#..#...#.#",
                 }.Joined();
 
-            var result = Day3.Part2(input);
+            var result = Day3.Part2(input, new []
+            {
+                (1, 1),
+                (3, 1),
+                (5, 1),
+                (7, 1),
+                (1, 2)
+            });
 
-            Assert.Equal(-1, result);
+            Assert.Equal(336UL, result);
         }
 
         [Fact]
@@ -61,14 +69,21 @@ namespace Tests
             Assert.Equal(193, result);
         }
 
-        [Fact(Skip = "WIP")]
+        [Fact]
         public void Solve_Part_2()
         {
             var input = Input.Day(3);
 
-            var result = Day3.Part2(input);
+            var result = Day3.Part2(input, new[]
+            {
+                (1, 1),
+                (3, 1),
+                (5, 1),
+                (7, 1),
+                (1, 2)
+            });
 
-            Assert.Equal(42, result);
+            Assert.Equal(1355323200UL, result);
         }
     }
 }

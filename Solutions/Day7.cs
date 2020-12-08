ce7d56791efa2a74cb57f5c1d4cd05fc5e23c76c
Rob Cooper <robc.the.geek@gmail.com>
2020-12-08 08:41:11 +0000

    WIP

--- Solutions/Day7.cs
+++ Solutions/Day7.cs
@@ -7,7 +7,45 @@ namespace Solutions
 {
     public static class Day7
     {
-        public static int Part1(string input) => -1;
+        public static int Part1(string input)
+        {
+            var queue = new Queue<string>();
+
+            var rules = input
+                .Split(Environment.NewLine)
+                .Select(s => {
+                    var r = new BagRule(s);
+                    if (r.Quantities.Any(x => x.BagColor == "shiny gold"))
+                        queue.Enqueue(r.BagColor);
+                    return r;
+                })
+                .ToDictionary(rule => rule.BagColor);
+
+            Console.Error.WriteLine($"{string.Join(", ", queue)}");
+
+            var count = 0;
+            while (queue.Count > 0)
+            {
+                // find anything that contains this bag, add to the queue.
+                var current = queue.Dequeue();
+                rules
+                    .Where(rule => rule.Value.Quantities.Any(qty => qty.BagColor == current))
+                    .Select(x => x.Key)
+                    .ToList()
+                    .ForEach(queue.Enqueue);
+
+                count++;
+            }
+
+            // keep track of all those that ahve "shiny gold bags" - we'll need these later.
+
+            // create list of bags that contain shiny gold bags
+
+            // ... recurse until no more container bags
+
+            return count;
+        }
+
         public static int Part2(string input) => -1;
 
     }
@@ -31,7 +69,7 @@ namespace Solutions
             foreach (Match match in contentRules)
             {
                 if (match.Groups["col"].Value == "no other bags") break;
-                
+
                 quantities.Add(new(
                     match.Groups["col"].Value,
                     int.Parse(match.Groups["qty"].Value)

b6320ec52a9e4658f442031345d635685d03e212
Rob Cooper <robc.the.geek@gmail.com>
2020-12-08 07:24:31 +0000

    Skeleton 7

---
+++ Tests/Day7Tests.cs
@@ -0,0 +1,28 @@
+using Solutions;
+using Xunit;
+
+namespace Tests
+{
+    public class Day7Tests
+    {
+        [Fact(Skip = "WIP")]
+        public void Solve_Part_1()
+        {
+            var input = Input.Day(7);
+
+            var result = Day7.Part1(input);
+
+            Assert.Equal(42, result);
+        }
+
+        [Fact(Skip = "WIP")]
+        public void Solve_Part_2()
+        {
+            var input = Input.Day(7);
+
+            var result = Day7.Part2(input);
+
+            Assert.Equal(42, result);
+        }
+    }
+}
